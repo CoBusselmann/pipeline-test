d02803c798c373806918de7de8d523c44962e1ec
CoBusselmann <oliver.busselmann@gmail.com>
2026-09-22 22:20:44 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Hej och välkommen!")
+
+name = input("Namn: ")
+age = int(input("Ålder: "))
+
+if age <= 18:
+    print(f"Hej {name}, som är {age}år gammal.\nDu får inte rösta.")
+else:
+     print(f"Hej {name}, som är {age}år gammal.\nDu får rösta")
