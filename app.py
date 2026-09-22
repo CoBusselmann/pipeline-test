b3cc9d8420f669e410779b8caa1ec06d99867fdc
CoBusselmann <oliver.busselmann@gmail.com>
2026-09-22 22:52:54 +0200

    Fixade newline i slutet

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 print("Hej och välkommen!")
-
 name = input("Namn: ")
 age = int(input("Ålder: "))
 
 if age <= 18:
     print(f"Hej {name}, som är {age}år gammal.\nDu får inte rösta.")
 else:
-     print(f"Hej {name}, som är {age}år gammal.\nDu får rösta")
+    print(f"Hej {name}, som är {age}år gammal.\nDu får rösta")
